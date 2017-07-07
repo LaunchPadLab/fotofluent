41db87c231c1e53c7de81d15e17fea2d7abd4c29
Rachel Killackey <rachel@launchpadlab.com>
2017-07-07 17:05:08 -0500

    adding vue, other files

---
+++ app/scripts/options.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+
+import Vue from 'vue'
+import Options from '../component/options.vue'
+
+const app = new Vue({
+  el: '#app',
+  render (h) {
+    return (
+      <Options />
+    )
+  }
+})
---
+++ app/scripts/tab.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+import Vue from 'vue'
+import Tab from '../component/tab.vue'
+import AsyncComputed from 'vue-async-computed'
+
+Vue.use(AsyncComputed)
+
+const app = new Vue({
+  el: '#app',
+  render (h) {
+    return (
+      <Tab></Tab>
+    )
+  }
+})
