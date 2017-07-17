11164eb26af1b6cc6758abb310a437c766c78dfd
dianacamacho <dcam1901@gmail.com>
2017-07-17 11:06:12 -0500

    add prop validation

--- app/components/translation.vue
+++ app/components/translation.vue
@@ -12,7 +12,9 @@
 
 <script>
   export default {
-    props: ['translation'],
+    props: {
+      translation: Object
+    },
     data() {
       return {
         foreignWord: this.translation.foreign_word,
