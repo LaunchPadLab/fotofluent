0835096368f0426770629e91cb501aa7d96a9686
Rachel Killackey <rachel@launchpadlab.com>
2017-07-18 10:24:22 -0500

    add options form, add vuex

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -2,10 +2,15 @@
 
 import 'babel-polyfill'
 import Vue from 'vue'
+import store from '../store/index.js'
 import Options from '../components/options.vue'
+import AsyncComputed from 'vue-async-computed'
+
+Vue.use(AsyncComputed)
 
 const app = new Vue({
   el: '#app',
+  store,
   render (h) {
     return (
       <Options />
---
+++ app/store/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import ChromePromise from 'chrome-promise'
+
+Vue.use(Vuex)
+const chromep = new ChromePromise()
+
+export const LANGUAGES = [
+  { key: 'GERMAN', value: 'GER' },
+  { key: 'SPANISH', value: 'SPA' }
+]
+
+const store = new Vuex.Store({
+  state: {
+    language: 'GER',
+  },
+
+  actions: {
+    async HYDRATE_STATE ({ commit }) {
+      const items = await chromep.storage.local.get([
+        'language'
+      ])
+      commit('SET_LANGUAGE', items.language || 'GER')
+    }
+  },
+
+  mutations: {
+    SET_LANGUAGE (state, language) {
+      state.language = language
+      chromep.storage.local.set({ language })
+    }
+  }
+})
+
+//
+// Hydrate on app start.
+//
+const hydrate = async () => {
+  await store.dispatch('HYDRATE_STATE')
+}
+hydrate()
+
+export default store
